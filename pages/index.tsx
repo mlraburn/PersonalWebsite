d6d2441f747b0991787d058d8a044de30c91c240
Matthew Raburn <matthew.raburn@gmail.com>
2025-06-27 23:58:06 -0400

    continuing to break each section up into its own component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Header from '../components/Header';
+import Hero from '../components/Hero';
 
 export default function HomePage(): JSX.Element {
   const [isDarkMode, setIsDarkMode] = useState(false);
@@ -7,8 +8,8 @@ export default function HomePage(): JSX.Element {
   return (
       <div className={`min-h-screen ${isDarkMode ? 'bg-slate-900' : 'bg-amber-50'}`}>
         <Header isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-        {/*<Hero />
-        <About />
+        <Hero />
+        {/*<About />
         <Timeline />
         <Projects />
         <Contact />
